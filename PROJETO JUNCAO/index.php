6ac3b3a72170d5931c9ab8afe4ddcd2ce44efc3c
RafaPeroni <99209499+RafaPeroni@users.noreply.github.com>
2023-03-06 11:05:46 -0300

    Add files via upload

---
+++ PROJETO JUNCAO/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="peroni" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PERONI JUNCAO</title>
+</head>
+<body>
+<h1>OLÁ MUNDO</h1>
+
+<?PHP
+echo "OLÁ MUNDO"
+?>
+
+<?php
+echo '<br>'. (3 * 3);
+echo '<br>'.(2 + 20);
+echo '<br>'.(10 + 227)
+ ?>
+
+ <?php
+ echo '<br>'. 'Esqueci o que era pra fazer mas ta ai, funcionando';
+ ?>
+
+</body>
+</html>
